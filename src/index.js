0ddb234822b073765431b9b1ce27f112b1fb9ae0
NicoleScott-web <nicolemilyscott@gmail.com>
2024-11-23 14:21:08 +1300

    FinalCommit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+require("dotenv").config();
+const dbconnect= require("./dbconnect")
+const app = express();
+
+// parse requests of content-type -
+app.use(express.json());
+app.get("/", (req, res) => {
+res.json({ message: "Welcome to mySQL application." });
+});
+let userRoutes = require('./routes/userRoutes');
+app.use('/api/users', userRoutes);
+let postRoutes = require('./routes/postRoutes');
+app.use('/api/posts', postRoutes);
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+console.log(`Server is running on port
+${PORT}.`);
+});
---
+++ src/models/index.js
@@ -0,0 +1,19 @@
+'use strict'
+const User = require('./user') //require the model
+const Post = require('./post')
+
+async function init() {
+await User.sync()
+await Post.sync(); // sync the model
+// also sync any extra models here
+};
+Post.belongsTo(User);
+User.hasMany(Post);
+
+init();
+
+module.exports = {
+User,
+Post, // export the model
+// also export any extra models here
+};
